cf4f8431eb1942301bbbccf585c1a53452303d32
nazrinbinyaminova <nazrinbinyaminova@gmail.com>
2023-03-30 14:05:48 +0400

    spring security

---
+++ src/main/java/org/example/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,10 @@
+package org.example.springsecurity;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
